d5bc31f600b8c4936bce8921dc6484c471947ffd
Supratik Roy <supratik1992@gmail.com>
2018-07-15 01:23:27 +0530

    removed sidebar, changed navbar style

--- src/app/components/partials/navbar/navbar.component.ts
+++ src/app/components/partials/navbar/navbar.component.ts
@@ -27,22 +27,4 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
   }
 
-  toggleNav() {
-    if (!this.open) {
-      document.getElementById('mySidenav').style.width = '250px';
-      // if (window.screen.width > 320) {
-      //   document.getElementById('main').style.marginLeft = '250px';
-      // }
-      document.getElementById('main').style.marginLeft = '250px';
-      this.open = true;
-    } else if (this.open) {
-      document.getElementById('mySidenav').style.width = '0';
-      // if (window.screen.width > 320) {
-      //   document.getElementById('main').style.marginLeft = '0';
-      // }
-      document.getElementById('main').style.marginLeft = '0';
-      this.open = false;
-    }
-  }
-
 }
